70c6cd2997186c3582971b70a7935836c11008da
rajpushp61 <rajpushp61@gmail.com>
2025-01-06 23:14:46 +0530

    megablog

---
+++ 11-project/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import authServie from "./appwrite/auth"
+import {login,logOut} from "./store/authSlice"
+import { Footer, Header } from './components'
+
+function App() {
+  
+  const [loading,setloading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authServie.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logOut())
+      }
+    })
+    .finally(() => setloading(false))
+  },[])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap  content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header />
+      <main>
+       TODO: {/* <Outlet /> */}
+      </main>
+      <Footer />
+
+      </div>
+      </div>
+  ) : null
+
+
+}
+
+export default App
