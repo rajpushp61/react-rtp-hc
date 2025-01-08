6e4ee82818408d4332d5c3a0c093642db363f034
rajpushp61 <rajpushp61@gmail.com>
2025-01-08 21:51:42 +0530

    updates

--- 11-project/src/conf/conf.js
+++ 11-project/src/conf/conf.js
@@ -2,8 +2,8 @@ const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID ),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_URL)
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 
